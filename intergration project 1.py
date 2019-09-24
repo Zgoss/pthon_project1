4bc305c738972e3046433e904fdcae70369a1d77
Zgoss <54559367+Zgoss@users.noreply.github.com>
2019-09-24 13:27:07 -0400

    Add files via upload

---
+++ intergration project 1.py
@@ -0,0 +1,25 @@
+# pthon_project1
+# Zachary Gossett
+# A quiz asking a variety of questions
+print ("Hello, welcome to the quiz. This quiz will ask a variety of questions, on a different number of topics. Lets begin. \n")
+score = 0
+answer1 =input ("Who currently has the most Surperbowl ring? \na. Emmit Smith \nb. Tom Brady \nc. Peyton Manning \nd. Eli Manning \nAnswer: ")
+if answer1 == "b" or answer1 == "Tom Brady":
+    score += 1
+    print("correct")
+    print("score: ", score)
+    print("\n")
+else:
+    print("Wrong! The answer is Tom Brady")
+    print("score: ", score)
+    print("\n")
+answer2 =input ("What is 4 + 5 * 3 / 3 equal to? \nAnswer: ")
+if answer2 == "9":
+    score += 1
+    print("correct")
+    print("score: ", score)
+    print("\n")
+else:
+    print("Wrong! The answer is 9 ")
+    print("score: ", score)
+    print("\n")
