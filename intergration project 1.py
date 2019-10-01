28ab8d052c569c4c32f3ac5bbb1f816672b02af8
Zgoss <54559367+Zgoss@users.noreply.github.com>
2019-10-01 14:44:25 -0400

    Update intergration project 1.py

--- intergration project 1.py
+++ intergration project 1.py
@@ -1,7 +1,7 @@
 # pthon_project1
 # Zachary Gossett
 # A quiz asking a variety of questions
-print ("Hello, welcome to the quiz. This quiz will ask a variety of questions, on a different number of topics. Lets begin. \n")
+print ("Hello, welcome to the quiz. This quiz will ask a variety of questions, on a different number of topics. Please answer in the letter of the option or the full name or number. Lets begin. \n")
 score = 0
 answer1 =input ("Who currently has the most Surperbowl ring? \na. Emmit Smith \nb. Tom Brady \nc. Peyton Manning \nd. Eli Manning \nAnswer: ")
 if answer1 == "b" or answer1 == "Tom Brady":
@@ -21,5 +21,15 @@ if answer2 == "9":
     print("\n")
 else:
     print("Wrong! The answer is 9 ")
+    print("score: ", score)
+    print("\n")
+answer3 =input ("Who has the most three poimters made in the NBA? \na. Reggie Miller \nb. Stephen Curry \nc. Ray Allen \nd. Kyle Korver \nAnswer: ")
+if answer3 == "c" or answer3 == "Ray Allen":
+    score += 1
+    print("correct")
+    print("score: ", score)
+    print("\n")
+else:
+    print("Wrong! The answer is Ray Allen")
     print("score: ", score)
     print("\n")
